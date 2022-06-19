0e140d83c417fcd73000147916811b6d730dd8e7
van-matos <vanmatos@poli.ufrj.br>
2022-06-19 16:22:14 -0300

    Rota /tweets - POST

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ app.use(express.json());
 app.use(cors());
 
 const users = [];
+const tweets = [];
 
 app.post('/sign-up', (req, res) => {
     const user = req.body;
     users.push(user);
+    res.send("OK");
+})
+
+app.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
     res.send("OK");
 })
 
